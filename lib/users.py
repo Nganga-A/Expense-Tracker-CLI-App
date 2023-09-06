2d69d443dcf95bc6ddbe9d2b790a2c03357b3e9a
Abed Nganga <ngangaabed@gmail.com>
2023-09-06 12:49:02 +0300

    Grouped the User Class Methods

--- lib/users.py
+++ lib/users.py
@@ -3,52 +3,54 @@ from models import User, Budget, Expense
 import bcrypt
 from datetime import datetime
 
-#create a user with hashed password
-def create_user(db: Session,username:str, password:bytes):
-    #hash the password before storing it
-    password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
-    user = User(username=username, password_hash=password_hash)
+class UserClassMethods:
+    @classmethod    #Create a user with a hashed password.
+    def create_user(cls, db: Session, username: str, password: bytes):
+        password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
+        user = User(username=username, password_hash=password_hash)
 
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
-
-#Retrieve a user by their ID.
-def get_user_by_id(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
-
-#Retrieve a user by their username.
-def get_user_by_username(db: Session, username: str):
-    return db.query(User).filter(User.username == username).first()
-
-#Retrieve the budgets associated with a user.
-def get_user_budgets(db: Session, user_id: int):
-    return db.query(Budget).filter(Budget.user_id == user_id).all()
-
-#Create a budget entry for a user.
-def create_user_budget(db: Session, user_id: int, category_id: int, amount: float):
-    budget = Budget(user_id=user_id, category_id=category_id, amount=amount)
-    db.add(budget)
-    db.commit()
-    db.refresh(budget)
-    return budget
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
 
 
-#Create an expense entry for a user.
-def create_user_expense(db: Session, user_id: int, category_id: int, date: datetime, description: str, amount: float):
-    expense = Expense(user_id=user_id, category_id=category_id, date=date, description=description, amount=amount)
-    
-    db.add(expense)
-    db.commit()
-    db.refresh(expense)
-    return expense
-
-# Retrieve expenses associated with a user.
-def get_user_expenses(db: Session, user_id: int):
-    return db.query(Expense).filter(Expense.user_id == user_id).all()
+    @classmethod  #Retrieve a user by their ID.
+    def get_user_by_id(cls, db: Session, user_id: int):
+        return db.query(User).filter(User.id == user_id).first()
 
 
+    @classmethod  #Retrieve a user by their username.
+    def get_user_by_username(cls, db: Session, username: str):
+        return db.query(User).filter(User.username == username).first()
+
+
+    @classmethod  #Retrieve the budgets associated with a user.
+    def get_user_budgets(cls, db: Session, user_id: int):
+        return db.query(Budget).filter(Budget.user_id == user_id).all()
+
+
+    @classmethod  #Create a budget entry for a user.
+    def create_user_budget(cls, db: Session, user_id: int, category_id: int, amount: float):
+        budget = Budget(user_id=user_id, category_id=category_id, amount=amount)
+        db.add(budget)
+        db.commit()
+        db.refresh(budget)
+        return budget
+
+
+    @classmethod  #Create an expense entry for a user.
+    def create_user_expense(cls, db: Session, user_id: int, category_id: int, date: datetime, description: str, amount: float):
+        expense = Expense(user_id=user_id, category_id=category_id, date=date, description=description, amount=amount)
+        db.add(expense)
+        db.commit()
+        db.refresh(expense)
+        return expense
+
+
+    @classmethod #Retrieve expenses associated with a user.
+    def get_user_expenses(cls, db: Session, user_id: int):
+        return db.query(Expense).filter(Expense.user_id == user_id).all()
 
 
 
